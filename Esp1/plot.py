958a394fe0c29c38fc0d245fbcdf2a05e1eac501
LorenzoBi <lorenzobiasi95@gmail.com>
2016-09-21 16:38:44 +0200

    Fished calculations

--- Esp1/plot.py
+++ Esp1/plot.py
@@ -21,6 +21,18 @@ def read_data(file):
     return t, vin, vout
 
 
+def read_data_final(file):
+    data = np.genfromtxt('data/' + file, delimiter=",",
+                         usecols=(0, 1, 2, 3), skip_header=2)
+    data = data[~np.isnan(data).any(axis=1)]
+    t = data[:, 0]
+    vin1 = data[:, 1]
+    vin2 = data[:, 2]
+    vout = data[:, 3]
+
+    return t, vin1, vin2, vout
+
+
 for file in files:
     t, vin, vout = read_data(file)
     plt.figure(file)
@@ -36,7 +48,17 @@ for file in files:
         amp_invertent_vout = max(vout)
         amp_invertent_vin = max(vin)
 
-
+t, vin1, vin2, vout = read_data_final('scope5.csv')
+plt.figure('scope5.csv')
+plt.grid(True)
+plt.plot(t, vin1)
+plt.plot(t, vin2)
+plt.plot(t, vout)
+plt.xlabel("time [s]")
+plt.ylabel("Voltage [V]")
+f_vin1 = max(vin1)
+f_vin2 = max(vin2)
+f_vout = max(vout)
 # plt.show()
 
 R2 = 218.371
@@ -44,7 +66,7 @@ R1 = 99.8913
 R3 = 99.8990
 R = [0, R1, R2, R3]
 dR = []
-cnt = 1
+cnt = 0
 for resitance in R:
     dR.append(get_error(resitance))
     print 'Resistance' + ' ' + str(cnt) + ': ' + str(resitance) + '+-' + str(get_error(resitance))
@@ -58,12 +80,27 @@ teo_follow_gain_vout = follow_gain_vin * (1 + R2_div_R1)
 amp_invertent_vout_err = err_prop(amp_invertent_vin, dv, R2_div_R1, R2_div_R1_err, '*')
 teo_amp_invertent_vout = amp_invertent_vin * R2_div_R1
 
+v1_div_r2 = f_vin1 / R[1]
+v2_div_r3 = f_vin2 / R[3]
+v1_div_r2_err = err_prop(f_vin1, dv, R[1], dR[1], '/')
+v2_div_r3_err = err_prop(f_vin2, dv, R[3], dR[3], '/')
+v_div_R = v1_div_r2 + v2_div_r3
+v_div_R_err = err_prop(v1_div_r2, v1_div_r2_err, v2_div_r3, v2_div_r3_err, '+')
+vout_teo = R1 * (v_div_R)
+vout_teo_err = err_prop(R[2], dR[2], v_div_R, v_div_R_err, '*')
+
 print 'Emitter follower:'
 print 'V_in', follow_gain_vin, '+-', dv
 print 'V_out', follow_gain_vout, '+-', dv
 print 'Theoric v_out', teo_follow_gain_vout, '+-', follow_gain_vout_err
 
-print  "Invertent amplifier"
+print "Invertent amplifier"
 print 'V_in', amp_invertent_vin, '+-', dv
 print 'V_out', amp_invertent_vout, '+-', dv
-print 'Theoric v_out', teo_amp_invertent_vout, '+-', teo_amp_invertent_vout
+print 'Theoric v_out', teo_amp_invertent_vout, '+-', amp_invertent_vout_err
+
+print 'Weighted summing amplifier'
+print 'V_in1', f_vin1, '+-', dv
+print 'V_in2', f_vin2, '+-', dv
+print 'V_out', f_vout, '+-', dv
+print 'Theoric v_out', vout_teo, '+-', vout_teo_err
